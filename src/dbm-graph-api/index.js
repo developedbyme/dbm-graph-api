1186365550b2c3ecc9e70b9f29dc9b1a381a2e99
Mattias Ekendahl <mattias@developedbyme.com>
2025-04-07 16:34:06 +0200

    Update: moved out tag manager

--- src/dbm-graph-api/index.js
+++ src/dbm-graph-api/index.js
@@ -565,15 +565,17 @@ export const setupSite = function(aServer) {
                     }
                 }
 
-				returnString += `<link rel="preconnect" href="${process.env.S3_BUCKET_PUBLIC_PATH}" crossorigin>
-				<!-- Google Tag Manager -->
-				<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-				})(window,document,'script','dataLayer','GTM-K8BL9Q5S');</script>
-				<!-- End Google Tag Manager -->
-				<link rel="stylesheet" type="text/css" href="${assetsUri}css/main.css?version=${version}" />
+				returnString += `<link rel="preconnect" href="${process.env.S3_BUCKET_PUBLIC_PATH}" crossorigin>`;
+
+                {
+                    let currentArray = site.injectCodeSnippets;
+                    let currentArrayLength = currentArray.length;
+                    for(let i = 0; i < currentArrayLength; i++) {
+                        returnString += currentArray[i];
+                    }
+                }
+
+				returnString += `<link rel="stylesheet" type="text/css" href="${assetsUri}css/main.css?version=${version}" />
 				<meta name="viewport" content="initial-scale=1,user-scalable=yes" />
 				<meta name="HandheldFriendly" content="true" />
 				<link rel="icon" type="image/png" href="${assetsUri}img/favicon.png">
@@ -622,17 +624,17 @@ export const setupSite = function(aServer) {
 		<meta property="og:type" content="website" />
 		<meta property="og:locale" content="${language}" />
 		<meta property="og:site_name" content="${siteName}" />
-		<meta property="og:title" content="${fields.title}" />
+		<meta property="og:title" content="${fields.title}" />`;
 
-		<!-- Google Tag Manager -->
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-K8BL9Q5S');</script>
-		<!-- End Google Tag Manager -->
+		{
+                    let currentArray = site.injectCodeSnippets;
+                    let currentArrayLength = currentArray.length;
+                    for(let i = 0; i < currentArrayLength; i++) {
+                        returnString += currentArray[i];
+                    }
+                }
 		
-		<link rel="stylesheet" type="text/css" href="${assetsUri}css/main.css?version=${version}" />
+		returnString += `<link rel="stylesheet" type="text/css" href="${assetsUri}css/main.css?version=${version}" />
 		
 		<link rel="icon" type="image/png" href="${assetsUri}img/favicon.png">`;
 
